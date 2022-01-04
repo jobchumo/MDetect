15862fa64aad5afb4250fa159234770ba360d0a7
cjkipu <jokipchirchir@gmail.com>
2022-01-04 15:27:04 +0300

    0.0.6

--- app/src/main/java/com/jobchumo/mddetect/TwitterActivity.java
+++ app/src/main/java/com/jobchumo/mddetect/TwitterActivity.java
@@ -1,9 +1,15 @@
 package com.jobchumo.mddetect;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.twitter.clientlib.ApiException;
 import com.twitter.clientlib.api.TwitterApi;
 import com.twitter.clientlib.model.GenericTweetsTimelineResponse;
@@ -16,11 +22,15 @@ import java.util.Set;
 import io.github.redouane59.twitter.TwitterClient;
 import io.github.redouane59.twitter.dto.tweet.Tweet;
 import io.github.redouane59.twitter.dto.tweet.TweetList;
+import io.github.redouane59.twitter.dto.user.User;
 import io.github.redouane59.twitter.signature.TwitterCredentials;
 
 public class TwitterActivity extends AppCompatActivity {
 
     protected  static TwitterClient twitterClient;
+    protected EditText username;
+    protected FirebaseUser firebaseUser;
+    protected FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,15 +44,34 @@ public class TwitterActivity extends AppCompatActivity {
                 .apiSecretKey("RzPVUxgeXYzgUfR8vrAsdRC0yEZqCa4KJkK9DXaNtOlXB0GV24")
                 .build());
 
-        twitterTweets();
+        username = findViewById(R.id.twitterUsername);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
     }
 
-    private void twitterTweets() {
+    private void twitterTweets(String userName) {
+
+        User user = twitterClient.getUserFromUserName(userName);
+        String id = user.getId();
 
         Tweet tweet = twitterClient.getTweet("1478303491177259008");
         System.out.println(tweet.getText());
 
-        TweetList userTimeline = twitterClient.getUserTimeline("717751924129472512");
+        TweetList userTimeline = twitterClient.getUserTimeline(id);
         System.out.println(userTimeline.getData());
+    }
+
+    public void userTimeline(View view) {
+        String userName = username.getText().toString().trim();
+
+        if (TextUtils.isEmpty(userName)) {
+            Toast.makeText(TwitterActivity.this, "Empty Field! \nPlease Enter Your Username", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else {
+            twitterTweets(userName);
+        }
+
     }
 }
