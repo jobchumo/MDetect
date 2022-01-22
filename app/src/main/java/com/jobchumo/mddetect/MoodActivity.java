aff9d16ee5dbfdc7860bcdc1460750da803bd1f8
cjkipu <jokipchirchir@gmail.com>
2022-01-22 18:17:02 +0300

    0.1.2

--- app/src/main/java/com/jobchumo/mddetect/MoodActivity.java
+++ app/src/main/java/com/jobchumo/mddetect/MoodActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
 public class MoodActivity extends AppCompatActivity {
 
+    String url = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mood);
+
+        determineMood();
+    }
+
+    private void determineMood() {
+
     }
 }
