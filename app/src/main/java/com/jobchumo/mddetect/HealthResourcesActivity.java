b4ca74c8621af771159dbd135ee3037541e3db10
cjkipu <jokipchirchir@gmail.com>
2022-01-23 23:51:43 +0300

    0.1.5

--- app/src/main/java/com/jobchumo/mddetect/HealthResourcesActivity.java
+++ app/src/main/java/com/jobchumo/mddetect/HealthResourcesActivity.java
@@ -4,14 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class HealthResourcesActivity extends AppCompatActivity {
+
+    protected ImageView mhVector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health_resources);
+
+        mhVector = findViewById(R.id.mh_vector);
+        mhVector.setClipToOutline(true);
+        
+        linkHelper();
+    }
+
+    private void linkHelper() {
+        TextView linkTextView = findViewById(R.id.line_five);
+        linkTextView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     public void backAction(View view) {
